9ae117b780d634466eef96a7576a7182066a5020
LivGy <livia.gyger@stud.fhgr.ch>
2023-10-26 13:23:59 +0200

    update look and style

--- js/scriptapi.js
+++ js/scriptapi.js
@@ -9,7 +9,7 @@ function parseRssFeed() {
     .then(data => {
     data.stories.forEach(story=> {
 // Hier kannst du die Karten erstellen und sie in deiner UI anzeigen
-    createCard(story.content.headline);
+    createCard(story.content);
       });
     })
     .catch(error => {
@@ -18,36 +18,39 @@ function parseRssFeed() {
 }
 
 // Funktion zum Erstellen einer Karte (Card) und Anzeigen in der UI
-// Damit es im HTML die Ihalte ausgibt
-function createCard(title, description, link) {
-  const rssFeedCards = document.getElementById('rssFeedCards');
-
+function createCard(content) {
+  // Hier kannst du die Logik zum Erstellen und Anzeigen der Karten in deiner UI implementieren
+  console.log(content);
   // Erstelle ein <div>-Element für die Karte
   const cardDiv = document.createElement('div');
-  cardDiv.className = 'card';
+  cardDiv.className = 'card'; // Füge CSS-Klassen hinzu, um das Styling anzupassen
 
-  // Füge ein Bild ein ober dem Titel
-  
+  // Erstelle ein <img>-Element für das featured_image
+  const featuredImageElement = document.createElement('img');
+  featuredImageElement.src = content.featured_image.filename; // Setze die Bildquelle (URL)
 
-  // Erstelle <h2> für den Titel
+  // Erstelle ein <h2>-Element für den Titel
   const titleElement = document.createElement('h4');
-  titleElement.textContent = title;
+  titleElement.textContent = content.headline;
 
-  // Erstelle <p> für die Beschreibung
+  // Erstelle ein <p>-Element für die Beschreibung
   const descriptionElement = document.createElement('p');
-  descriptionElement.textContent = description;
+  descriptionElement.textContent = content.featured_excerpt;
 
-  // Erstelle <a> für den Link
+
+  // Erstelle ein <a>-Element für den Link
   const linkElement = document.createElement('a');
   linkElement.textContent = 'Mehr lesen';
-  linkElement.href = link;
+  linkElement.href = content.link;
 
   // Füge die erstellten Elemente zur Karte hinzu
+  cardDiv.appendChild(featuredImageElement);
   cardDiv.appendChild(titleElement);
   cardDiv.appendChild(descriptionElement);
   cardDiv.appendChild(linkElement);
 
-  // Füge die Karte zum Container hinzu
+  // Füge die Karte zum Container hinzu (z.B., einem Container mit der ID 'rssFeedCards')
+  const rssFeedCards = document.getElementById('rssFeedCards');
   rssFeedCards.appendChild(cardDiv);
 }
 
