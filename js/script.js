bf93041064af90a86bd5ffcf181eecf262fbab6f
LivGy <livia.gyger@stud.fhgr.ch>
2023-11-13 10:21:14 +0100

    update several documents

--- js/script.js
+++ js/script.js
@@ -36,3 +36,38 @@ $(function() {
     $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
   });
   });
+
+
+
+/*****************ZITATE SLIDESHOW******************/
+
+let slideIndex2 = 1;
+showSlides2(slideIndex2);
+
+function plusSlides2(n) {
+  showSlides2(slideIndex2 += n);
+}
+
+function currentSlide2(n) {
+  showSlides2(slideIndex2 = n);
+}
+
+function showSlides2(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides2");
+  let dots = document.getElementsByClassName("dot2");
+  if (n > slides.length) {slideIndex2 = 1}
+  if (n < 1) {slideIndex2 = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex2-1].style.display = "block";
+}
+
+// Automatische Diashow nur für die ZWEITE Slideshow
+setInterval(function() {
+  plusSlides2(1);
+}, 7000); // Ändere die Zeit (in Millisekunden) zwischen den Bildern hier
